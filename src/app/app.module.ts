a589d2f31ddedc09ce2497d9457d1e43a0751282
Arsen <arsenyeghiazarian@gmail.com>
2018-12-06 16:17:51 +0400

    page container created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { NgBulmaLibModule } from 'ng-bulma-lib';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { SectionContainerComponent } from './section-container/section-container.component';
+import { PageContainerComponent } from './page-container/page-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    SectionContainerComponent
+    SectionContainerComponent,
+    PageContainerComponent
   ],
   imports: [
     BrowserModule,

ec2c91d3dfdab6b368e82f2f713eeaae386d1625
Vahagn Yavruyan <vahagyavruyan@gmail.com>
2018-12-26 12:22:23 +0400

    main screen responsive

--- src/app/pages/development/development.component.ts
+++ src/app/pages/development/development.component.ts
@@ -1,22 +1,23 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { flatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-development',
   templateUrl: './development.component.html',
   styleUrls: ['./development.component.scss']
 })
-export class DevelopmentTypesComponent implements OnInit {
-  pageName;
+export class DevelopmentTypesComponent {
   pageData = null;
 
   constructor(private route: ActivatedRoute, private http: HttpClient) {
-    this.route.data.subscribe(res => this.pageName = res.name);
-  }
-
-  ngOnInit() {
-    this.http.get(`assets/${this.pageName}.json`)
-      .subscribe(res => this.pageData = res);
+    this.route.data
+      .pipe(
+        flatMap( res => {
+          return this.http.get(`assets/${res.name}.json`);
+        })
+      )
+      .subscribe(data => this.pageData = data);
   }
 }
